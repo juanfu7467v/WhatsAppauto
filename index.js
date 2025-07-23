f76128115136598513d6cab1d166bd72195c0e3e
juanfu7467v <movistarcervise@gmail.com>
2025-07-23 01:12:00 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const express = require("express");
-const path = require("path");
+const { createCanvas } = require("canvas");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 8080;
 
-app.use("/static", express.static(path.join(__dirname, "static")));
+app.get("/imagen", (req, res) => {
+  const texto = req.query.texto || "Hola Mundo";
 
-app.get("/", (req, res) => {
-  res.send(`
-    <html>
-      <head><title>QR Bot Pyrogram</title></head>
-      <body style="text-align:center;padding-top:50px;">
-        <h1>Escanea el QR desde tu app</h1>
-        <img src="/static/qr.png" style="width:300px;height:auto;" />
-      </body>
-    </html>
-  `);
+  const canvas = createCanvas(400, 200);
+  const ctx = canvas.getContext("2d");
+
+  // Fondo blanco
+  ctx.fillStyle = "#ffffff";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  // Texto negro
+  ctx.fillStyle = "#000000";
+  ctx.font = "bold 30px Arial";
+  ctx.fillText(texto, 50, 100);
+
+  res.setHeader("Content-Type", "image/png");
+  canvas.pngStream().pipe(res);
 });
 
 app.listen(PORT, () => {
